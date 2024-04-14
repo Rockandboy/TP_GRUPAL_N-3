68eb2ece806b95b71cac2375cb1f94f9bf589921
Rockandboy <Rockandboy.fl@gmail.com>
2024-04-14 17:02:48 -0300

    Evitar Repeticiones y Items precargados
    Hola chicos ahí hice mi aporte, en este caso hice dos cosas
    - Cree una funcion booleana llamada Loc_Repetida, recibe el string que nosotros queremos cargar y toma el trabajo de revisar que no este repetido y lo hace por cada item ingresado en el dropdownlist.
    - Precargue tambien unos items en ddlLocalidades que son las siguientes: "Roque perez, Gral. Pacheco y Lobos".

--- Ejercicio1_TP3/Ejercicio1_TP3/FormEJ1_TP3.aspx.cs
+++ Ejercicio1_TP3/Ejercicio1_TP3/FormEJ1_TP3.aspx.cs
@@ -9,9 +9,26 @@ namespace Ejercicio1_TP3
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        private bool Loc_Repetida(string localidad)
+        {
+            foreach (ListItem i in ddlLocalidades.Items)
+            {
+                if (i.ToString() == localidad)
+                { 
+                    return false;
+                }
+            }
+            return true;
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(IsPostBack == false)
+            {
+            ddlLocalidades.Items.Add("Roque Perez");
+            ddlLocalidades.Items.Add("Gral. Pacheco");
+            ddlLocalidades.Items.Add("Lobos");
+            }
         }
 
         protected void btGuardarLoc_Click(object sender, EventArgs e)
@@ -19,24 +36,27 @@ namespace Ejercicio1_TP3
            
             // Agrega la localidad al DropDownList
             string localidad = txtLocalidad.Text.Trim();
+
+            if(Loc_Repetida(localidad) == true)
+            { 
             ddlLocalidades.Items.Add(new ListItem(localidad, localidad));
+            }
+            
 
             if (txtLocalidad.Text.Length > 0)
             {
              txtLocalidad.Text = "";
-            
-            
             }
         }
         protected void txtLocalidad_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
         protected void ddlLocalidades_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
-          //  string localidadSeleccionada = ddlLocalidades.SelectedValue;
-          
+
+            //  string localidadSeleccionada = ddlLocalidades.SelectedValue;
+
         }
     }
 }
